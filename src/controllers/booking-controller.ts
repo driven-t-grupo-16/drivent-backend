f767eb9030aff4bfefde4d67ec67772f829b5360
RayAra2004 <raynan.araujo@gmail.com>
2023-10-28 18:01:48 -0300

    feat: Login with GitHub

--- src/controllers/booking-controller.ts
+++ src/controllers/booking-controller.ts
@@ -4,29 +4,29 @@ import { AuthenticatedRequest } from '@/middlewares';
 import { bookingService } from '@/services';
 
 export async function getBooking(req: AuthenticatedRequest, res: Response) {
-    const { userId } = req;
-    const booking = await bookingService.getBooking(userId);
+  const { userId } = req;
+  const booking = await bookingService.getBooking(userId);
 
-    return res.status(httpStatus.OK).send({
-        id: booking.booking.id,
-        Room: booking.booking.Room,
-    });
+  return res.status(httpStatus.OK).send({
+    id: booking.booking.id,
+    Room: booking.booking.Room,
+  });
 }
 
 export async function bookRoom(req: AuthenticatedRequest, res: Response) {
-    const { userId } = req;
-    const roomId = Number(req.body.roomId);
+  const { userId } = req;
+  const roomId = Number(req.body.roomId);
 
-    const booking = await bookingService.bookRoomById(userId, roomId);
+  const booking = await bookingService.bookRoomById(userId, roomId);
 
-    return res.status(httpStatus.OK).send({ bookingId: booking.id });
+  return res.status(httpStatus.OK).send({ bookingId: booking.id });
 }
 
 export async function changeBooking(req: AuthenticatedRequest, res: Response) {
-    const { userId } = req;
-    const roomId = Number(req.body.roomId);
+  const { userId } = req;
+  const roomId = Number(req.body.roomId);
 
-    const booking = await bookingService.changeBookingRoomById(userId, roomId);
+  const booking = await bookingService.changeBookingRoomById(userId, roomId);
 
-    return res.status(httpStatus.OK).send({ bookingId: booking.id });
+  return res.status(httpStatus.OK).send({ bookingId: booking.id });
 }
